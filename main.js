05b87a3aaafdbe20b2ab85535cc07c2cd1a56d12
oliveraladrovic <oliver.aladrovic@gmail.com>
2021-04-02 23:36:46 +0200

    Manual and autoplay carousel, full screen only. Add responsiveness and content inside

--- main.js
+++ main.js
@@ -4,3 +4,33 @@ const navbar = document.querySelector('.navbar');
 menuIcon.addEventListener('click', () => {
   navbar.classList.toggle('show-nav');
 });
+
+// Home page slider
+const slides = document.querySelectorAll('.slide');
+const btns = document.querySelectorAll('.slider-btn');
+let currentSlide = 0;
+// Manual navigation
+const showSlide = (currentSlide) => {
+  slides.forEach((slide) => {
+    slide.classList.remove('active-slide');
+    btns.forEach((btn) => {
+      btn.classList.remove('active-slider-btn');
+    });
+  });
+  slides[currentSlide].classList.add('active-slide');
+  btns[currentSlide].classList.add('active-slider-btn');
+};
+btns.forEach((btn, i) => {
+  btn.addEventListener('click', () => {
+    showSlide(i);
+    currentSlide = i;
+  });
+});
+// Autoplay
+setInterval(() => {
+  currentSlide++;
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  showSlide(currentSlide);
+}, 10000);
